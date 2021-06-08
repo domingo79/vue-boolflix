115a6160e77fad27d0365fff64ceb8558b21df9c
domingo79 <andria140779@yahoo.it>
2021-06-08 17:43:29 +0200

    gestione errori e chiamata

--- Milestone-1/assset/js/main.js
+++ Milestone-1/assset/js/main.js
@@ -1,9 +1,27 @@
 const app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        url: 'https://api.themoviedb.org/3/search/movie',
+        apiKey: 'a036838588a8580d8706b9e66b467405',
+        query: '',
+        movies: [],
+        error: '',
     },
-    methods: {},
+    methods: {
+        serch() {
+            let chiamata = `${this.url}?api_key=${this.apiKey}&query=${this.query}`
+            axios
+                .get(chiamata)
+                .then(respose => {
+                    console.log(respose.data.results);
+                    this.movies = respose.data.results;
+                })
+                .catch(err => {
+                    console.error(err);
+                    this.error = 'Sorry.. ' + err
+                });
+        }
+    },
     computed: {},
     watch: {},
     mounted() { }
